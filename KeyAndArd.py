1347463b197d14c3790bb8e9130c5bb51cc74fc1
nekrodezynfekator <marcinjukiewicz@gmail.com>
2018-03-28 22:44:06 +0200

    Release candidate
    tests needed

--- KeyAndArd.py
+++ KeyAndArd.py
@@ -3,11 +3,6 @@ import serial
 import pygame
 import multiprocessing as mp
 
-'''
-Background (child) process to collect the data.
-Main (paret) procesess to watch for keystrokes.
-'''
-
 
 def mp_data_collect(state, curr_time):
 
@@ -18,7 +13,6 @@ def mp_data_collect(state, curr_time):
         dataSer = ser.readline()
 
         with open(output_path, 'a') as f:
-            # TODO pass strip from child process (instead of `s`).
             row = '%s,%s,%s\n' % (dataSer.strip(), state.value, curr_time.value)
             f.write(row)
 
@@ -32,18 +26,9 @@ terminate = mp.Event()
 state = mp.Value('i', 0)
 curr_time = mp.Value('i', 0)
 
-# `state` should be like:
-# state = mp.Value('c', 0)
-# But i don't know how to get `1 byte char` in python.
-# See:
-# https://docs.python.org/2/library/multiprocessing.html#multiprocessing.Value
-# and:
-# https://docs.python.org/2/library/array.html#module-array
-
 collector = mp.Process(name='proc_acq', target=mp_data_collect,
                        args=(state, curr_time,))
 collector.start()
-
 
 pygame.init()
 time.sleep(2)
@@ -56,10 +41,20 @@ while not terminate.is_set():
     curr_time.value = int(round((time.time()-start)*1000,0))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_q:
                 state.value = 1
-            elif event.key == pygame.K_m:
+            elif event.key == pygame.K_w:
                 state.value = 2
+            elif event.key == pygame.K_e:
+                state.value = 3
+            elif event.key == pygame.K_a:
+                state.value = 4
+            elif event.key == pygame.K_s:
+                state.value = 5
+            elif event.key == pygame.K_d:
+                state.value = 6
+            elif event.key == pygame.K_m:
+                state.value = 7
                 terminate.set()
         else:
             state.value = 0
