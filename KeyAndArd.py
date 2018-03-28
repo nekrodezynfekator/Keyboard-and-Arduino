b131b7c2eaf7d93fa8ba0440a32b050900bce90b
mikbuch <mikolaj.buchwald@gmail.com>
2018-03-28 21:53:47 +0200

    Almost working, this should have this construction.

--- KeyAndArd.py
+++ KeyAndArd.py
@@ -1,52 +1,69 @@
-import thread
 import time
 import serial
 import pygame
+import multiprocessing as mp
 
-state = '_'
-
-def keyboardRead(threadName, delay):
-    global state
-    pygame.init()
-    time.sleep(5)
-    screen = pygame.display.set_mode((50, 50))
-    while state != 'm':
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                     state = 'a'
-                elif event.key == pygame.K_m:
-                    state = 'm'
-            else:
-                state = '_'
-
-    pygame.quit()
-    
-def arduinoRead( threadName, start):
-    global state
-    data=[]
-    start=time.time()
-    ser = serial.Serial('/dev/ttyUSB0', 9600)
-    while state != 'm':
-        dataSer=ser.readline() 
-        data.append(dataSer.strip() + ', ' + str(state) + ', ' +str("%.2f"%(time.time()-start)) +'\n')  
-
-    ser.close()
-    saveData(data)
-
-def saveData(data):
-    textFile = open('test.txt','w')
-    textFile.writelines(data)
-    textFile.close()
+'''
+Background (child) process to collect the data.
+Main (paret) procesess to watch for keystrokes.
+'''
 
 
-try:
-   thread.start_new_thread( arduinoRead, ("Thread-1", 2, ) )
-   thread.start_new_thread( keyboardRead, ("Thread-2", 4, ) )
-except:
-   print "Error: unable to start thread"
+def mp_data_collect(state):
 
-while 1:
-    pass
+    # ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+    while state.value != 1:
+        state.value = 2
+
+    #     dataSer = ser.readline()
+    #     state.value = dataSer.strip()
+    #
+    # ser.close()
 
 
+output_path = 'test.txt'
+
+state = mp.Value('i', 0)
+
+# Should be:
+state = mp.Value('c', 0)
+# But i don't know how to get `1 byte char` in python.
+# See:
+# https://docs.python.org/2/library/multiprocessing.html#multiprocessing.Value
+# and:
+# https://docs.python.org/2/library/array.html#module-array
+
+collecter = mp.Process(name='proc_acq', target=mp_data_collect, args=(state,))
+collecter.start()
+
+
+pygame.init()
+time.sleep(2)
+print('started')
+
+start = time.time()
+
+screen = pygame.display.set_mode((50, 50))
+while state.value != 2:
+    # FIXME: doesn't listens keystrokes, I don't know why yet.
+    # It has sth to with the way int is stored in the `state` variable.
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                state.value = 1
+            elif event.key == pygame.K_m:
+                state.value = 2
+        else:
+            state.value = 0
+
+    curr_time = '%.4f' % (time.time()-start)
+
+    with open(output_path, 'a') as f:
+        # TODO pass strip from child process (instead of `s`).
+        row = '%s,%s,%s\n' % ('s', state.value, curr_time)
+        f.write(row)
+
+collecter.join()
+
+pygame.quit()
