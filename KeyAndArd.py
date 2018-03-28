77b1357dfea9ba7eecf34cf7008a4a087e76c9c9
mikbuch <mikolaj.buchwald@gmail.com>
2018-03-28 22:02:52 +0200

    Some comments and further changes, still not working.

--- KeyAndArd.py
+++ KeyAndArd.py
@@ -9,33 +9,40 @@ Main (paret) procesess to watch for keystrokes.
 '''
 
 
-def mp_data_collect(state):
+def mp_data_collect(state, curr_time):
 
-    # ser = serial.Serial('/dev/ttyUSB0', 9600)
+    ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-    while state.value != 1:
-        state.value = 2
+    while not terminate.is_set():
 
-    #     dataSer = ser.readline()
-    #     state.value = dataSer.strip()
-    #
-    # ser.close()
+        dataSer = ser.readline()
+
+        with open(output_path, 'a') as f:
+            # TODO pass strip from child process (instead of `s`).
+            row = '%s,%s,%s\n' % (dataSer, state.value, curr_time.value)
+            f.write(row)
+
+    ser.close()
 
 
 output_path = 'test.txt'
 
-state = mp.Value('i', 0)
+terminate = mp.Event()
 
-# Should be:
-state = mp.Value('c', 0)
+state = mp.Value('i', 0)
+curr_time = mp.Value('i', 0)
+
+# `state` should be like:
+# state = mp.Value('c', 0)
 # But i don't know how to get `1 byte char` in python.
 # See:
 # https://docs.python.org/2/library/multiprocessing.html#multiprocessing.Value
 # and:
 # https://docs.python.org/2/library/array.html#module-array
 
-collecter = mp.Process(name='proc_acq', target=mp_data_collect, args=(state,))
-collecter.start()
+collector = mp.Process(name='proc_acq', target=mp_data_collect,
+                       args=(state, curr_time,))
+collector.start()
 
 
 pygame.init()
@@ -45,7 +52,10 @@ print('started')
 start = time.time()
 
 screen = pygame.display.set_mode((50, 50))
-while state.value != 2:
+while not terminate.is_set():
+    # FIXME in milliseconds so that it can be stored as int, now it will
+    # throw an error.
+    curr_time.value = '%.4f' % (time.time()-start)
     # FIXME: doesn't listens keystrokes, I don't know why yet.
     # It has sth to with the way int is stored in the `state` variable.
     for event in pygame.event.get():
@@ -54,16 +64,9 @@ while state.value != 2:
                 state.value = 1
             elif event.key == pygame.K_m:
                 state.value = 2
+                terminate.set()
         else:
             state.value = 0
 
-    curr_time = '%.4f' % (time.time()-start)
-
-    with open(output_path, 'a') as f:
-        # TODO pass strip from child process (instead of `s`).
-        row = '%s,%s,%s\n' % ('s', state.value, curr_time)
-        f.write(row)
-
-collecter.join()
-
+collector.join()
 pygame.quit()
